2fc1dd3440c84c5744f5735ffefa6486cccb124b
osamaSale <osama.mohamad.salem@gmail.com>
2024-06-22 06:34:30 +0300

    commit1

--- 11-MySql/server.js
+++ 11-MySql/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoutes');
 const cors = require("cors");
-require("dotenv").config();
+require("dotenv").config();z
 
 const app = express();
 const PORT = process.env.PORT || 5000;
